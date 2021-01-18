0e5ef5a380877366a210e9ceca64a7166c29efb5
Chris-Kin <misuhoom@gmail.com>
2021-01-18 21:56:05 +0800

    chore:rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,30 @@
 import typescript from "rollup-plugin-typescript";
+import copy from 'rollup-plugin-copy';
+import { terser } from "rollup-plugin-terser";
 
-export default {
-    input: 'src/main.ts',
+export default [
+  {
+    input: 'src/script/popup.js',
     plugins: [
-        typescript({
-            exclude: "node_modules/**",
-            typescript: require("typescript")
-        }),
+      typescript({
+        exclude: "node_modules/**",
+        typescript: require("typescript")
+      }),
+      copy({
+        targets: [
+          { src: 'src/asset/*', dest: 'dist/asset/' },
+          { src: 'src/html/*', dest: 'dist/html/' },
+          { src: 'src/style/*', dest: 'dist/style/' },
+          { src: 'src/manifest.json', dest: 'dist/' },
+        ]
+      }),
     ],
     output: [
-        {
-            format: "cjs",
-            file: "lib/bundle.cjs.js"
-        },
-        {
-            format: "esm",
-            file: "lib/bundle.esm.js"
-        },
-        {
-            format: "umd",
-            name: "cookie-master",
-            file: "lib/bundle.umd.js"
-        },
+      {
+        format: "esm",
+        file: "dist/script/popup.js",
+        plugins: process.env.NODE_ENV === 'prod' ? [terser()] : []
+      },
     ]
-};
+  },
+];
--- typings/global.d.ts
+++ typings/global.d.ts
@@ -2,7 +2,7 @@ export {};
 
 declare global {
     interface Window {
-        chrome: string;
+        chrome: any;
     }
 
     type Nullable<T> = T | null;
