c7e42e0f004b08d0fa9ffa71b33765c6372abce6
于明严 <yumingyan@bytedance.com>
2024-05-04 22:49:49 +0800

    feat: init

--- popup.tsx
+++ popup.tsx
@@ -1,25 +1,358 @@
-import { useState } from "react"
+import AddImg from "data-base64:~assets/add.svg"
+import CheckImg from "data-base64:~assets/check.svg"
+import CopyImg from "data-base64:~assets/copy.svg"
+import DeleteImg from "data-base64:~assets/delete.svg"
+import EmptyImg from "data-base64:~assets/empty.svg"
+import HelpImg from "data-base64:~assets/help.svg"
+import ImportImg from "data-base64:~assets/import.svg"
+import UncheckImg from "data-base64:~assets/uncheck.svg"
+import { useCallback, useEffect, useRef, useState } from "react"
+
+import "./popup.css"
+
+import {
+  copy,
+  generateURL,
+  getCookies,
+  Loading,
+  Message,
+  removeCookiesAsync,
+  setCookiesAsync
+} from "./util/shared.js"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [cookieList, setCookieList] = useState([])
+  const cookieNameRef = useRef(null)
+  const [isAdding, setIsAdding] = useState(false)
+  const [addingCookie, setAddingCookie] = useState({
+    name: "",
+    value: ""
+  })
+
+  const [canAddingCookieSubmit, setCanAddingCookieSubmit] = useState(false)
+  useEffect(() => {
+    setCanAddingCookieSubmit(Boolean(addingCookie.name && addingCookie.value))
+  }, [addingCookie.name, addingCookie.value])
+
+  useEffect(() => {
+    getCookies().then((c) => setCookieList(c))
+  }, [])
+
+  useEffect(() => {
+    if (isAdding === false) {
+      setAddingCookie({
+        name: "",
+        value: ""
+      })
+    } else {
+      setTimeout(() => {
+        cookieNameRef.current.focus()
+      }, 0)
+    }
+    // /** enter keyboard listenr */
+    // document.addEventListener("keydown", keydownListener, false)
+    // return () => {
+    //   document.removeEventListener("keydown", keydownListener)
+    // }
+  }, [isAdding])
+
+  const keydownListener = (e) => {
+    if (e.key === "Enter" && isAdding) {
+      onAdd()
+    }
+  }
+
+  const onImport = async () => {
+    const loading = Loading()
+    const input = document.createElement("input")
+    input.style.height = "0px"
+    input.style.border = "none"
+    input.style.position = "absolute"
+    document.body.appendChild(input)
+    input.focus()
+    document.execCommand("paste")
+    try {
+      let cookieFromClipBoard
+      try {
+        cookieFromClipBoard = JSON.parse(input.value)
+      } catch {
+        cookieFromClipBoard = ""
+      }
+
+      if (!Array.isArray(cookieFromClipBoard)) {
+        Message({
+          type: "warn",
+          text: "please paste a cookie array"
+        })
+        return
+      }
+      await Promise.all(
+        cookieFromClipBoard.map((el) => {
+          return setCookiesAsync(el, true)
+        })
+      )
+      Message({ type: "success", text: "data imported successfully" })
+    } catch (err) {
+      Message({ type: "error", text: "some cookie set error" })
+    } finally {
+      getCookies().then((c) => setCookieList(c))
+      ;(loading as any).close()
+      document.body.removeChild(input)
+    }
+  }
+
+  const onCopy = () => {
+    copy(JSON.stringify(cookieList))
+    Message({ type: "success", text: "cookies have been copied" })
+  }
+
+  const onSingleCopy = (i) => {
+    copy(JSON.stringify([cookieList[i]]))
+    Message({ type: "success", text: "cookie has been copied" })
+  }
+
+  const onFlatCopy = () => {
+    const text = cookieList.reduce((collect, cur) => {
+      return collect + `${cur.name}=${cur.value}; `
+    }, "")
+    copy(text.replace(/;\s$/, ""))
+    Message({ type: "success", text: "cookies has been copied in flat form" })
+  }
+
+  const onClear = () => {
+    cookieList.forEach((el) => {
+      const url = generateURL(el)
+      removeCookiesAsync({ name: el.name, url })
+    })
+    setCookieList([])
+    Message({ type: "success", text: `all cookies has been deleted` })
+  }
+
+  const onAdd = async () => {
+    if (!canAddingCookieSubmit) {
+      Message({ type: "warn", text: "please input cookie's name and value" })
+      return
+    }
+    const tabs = await chrome.tabs.query({ currentWindow: true, active: true })
+    const host = new URL(tabs?.[0].url).host
+    const hostWithoutPort = host.split(":")[0]
+
+    const cookie = {
+      name: addingCookie.name,
+      value: addingCookie.value,
+      domain: hostWithoutPort,
+      expirationDate: new Date("2048-10-24").getTime(),
+      path: "/"
+    }
+    setCookiesAsync(cookie, true)
+      .then(() => {
+        Message({ type: "success", text: "success" })
+        getCookies().then((c) => setCookieList(c))
+        setIsAdding(false)
+      })
+      .catch(() => {
+        Message({ type: "error", text: "failed" })
+      })
+  }
+
+  const onDelete = async (i) => {
+    const cookie = cookieList[i]
+    const url = generateURL(cookie)
+    await removeCookiesAsync({ name: cookie.name, url })
+    getCookies().then((c) => setCookieList(c))
+    Message({ type: "success", text: `${cookie.name} has been deleted` })
+  }
+
+  const toggleSecure = async (i) => {
+    const cookie = cookieList[i]
+
+    // fixed: can not set a secure cookie to unsecure tab, so delete it first
+    if (cookie.secure) {
+      const url = generateURL(cookie)
+      await removeCookiesAsync({ name: cookie.name, url })
+    }
+
+    cookie.secure = !cookie.secure
+
+    setCookiesAsync(cookie)
+      .then(() => {
+        Message({ type: "success", text: "success" })
+      })
+      .catch(() => {
+        Message({ type: "error", text: "failed" })
+      })
+      .finally(() => {
+        getCookies().then((c) => setCookieList(c))
+      })
+  }
+
+  const toggleHttpOnly = async (i) => {
+    const cookie = cookieList[i]
+    cookie.httpOnly = !cookie.httpOnly
+
+    setCookiesAsync(cookie)
+      .then(() => {
+        Message({ type: "success", text: "success" })
+      })
+      .catch(() => {
+        Message({ type: "error", text: "failed" })
+      })
+      .finally(() => {
+        getCookies().then((c) => setCookieList(c))
+      })
+  }
+
+  const handleNameInput = (e: any) => {
+    setAddingCookie({
+      ...addingCookie,
+      name: e.target.value
+    })
+  }
+
+  const handleValueInput = (e: any) => {
+    setAddingCookie({
+      ...addingCookie,
+      value: e.target.value
+    })
+  }
+
+  if (isAdding) {
+    return (
+      <div id="app">
+        <div className="add-box">
+          <div className="add-item">
+            <label>name</label>
+            <input
+              type="text"
+              ref={cookieNameRef}
+              value={addingCookie.name}
+              onChange={handleNameInput}
+            />
+          </div>
+          <div className="add-item">
+            <label>value</label>
+            <input
+              type="text"
+              value={addingCookie.value}
+              onChange={handleValueInput}
+            />
+          </div>
+          <div className="footer">
+            <button onClick={() => setIsAdding(false)}>cancel</button>
+            <button
+              disabled={!canAddingCookieSubmit}
+              className="positive"
+              onClick={onAdd}>
+              ok
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
 
   return (
-    <div
-      style={{
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
-    </div>
+    <>
+      <div id="app">
+        <div className="operation">
+          <a
+            className="help"
+            href="https://chris-kin.github.io/cookie-master-doc/"
+            target="_blank"
+            rel="noopener noreferrer">
+            <img title="help documents" src={HelpImg} />
+          </a>
+          <button title="Copy all cookies" onClick={onCopy}>
+            <img src={CopyImg} />
+            copy
+          </button>
+          <button title="Copy with flat data structure" onClick={onFlatCopy}>
+            <img src={CopyImg} />
+            flat copy
+          </button>
+          <button title="Clear all cookies" onClick={onClear}>
+            <img src={DeleteImg} />
+            clear
+          </button>
+          <button
+            title="Import cookies in current root domain which in standard structure"
+            onClick={onImport}>
+            <img src={ImportImg} />
+            import
+          </button>
+          <button
+            title="create a new cookie in current domain"
+            onClick={() => setIsAdding(true)}>
+            <img src={AddImg} />
+            add
+          </button>
+        </div>
+        <div className="cookie">
+          <div className="cookie-item">
+            <div>Name</div>
+            <div>Value</div>
+            <div>Domain</div>
+            <div className="cookie-item-http">Http Only</div>
+            <div className="cookie-item-secure">Secure</div>
+          </div>
+          {!cookieList.length ? (
+            <img className="empty-img" src={EmptyImg} alt="" />
+          ) : (
+            cookieList.map((el, i) => {
+              return (
+                <div className="cookie-item" key={`${el.name}${el.value}`}>
+                  <div title={el.name}>{el.name}</div>
+                  <div title={el.value}>{el.value}</div>
+                  <div>{el.domain}</div>
+                  <div className="cookie-item-http">
+                    {el.httpOnly ? (
+                      <img
+                        onClick={() => toggleHttpOnly(i)}
+                        src={CheckImg}
+                        alt=""
+                      />
+                    ) : (
+                      <img
+                        onClick={() => toggleHttpOnly(i)}
+                        src={UncheckImg}
+                        alt=""
+                      />
+                    )}
+                  </div>
+                  <div className="cookie-item-secure">
+                    {el.secure ? (
+                      <img
+                        onClick={() => toggleSecure(i)}
+                        src={CheckImg}
+                        alt=""
+                      />
+                    ) : (
+                      <img
+                        onClick={() => toggleSecure(i)}
+                        src={UncheckImg}
+                        alt=""
+                      />
+                    )}
+                  </div>
+                  <section className="cookie-item-delete">
+                    <img
+                      src={CopyImg}
+                      title="copy this cookie"
+                      onClick={() => onSingleCopy(i)}
+                    />
+                    <img
+                      src={DeleteImg}
+                      title="delete this cookie"
+                      onClick={() => onDelete(i)}
+                    />
+                  </section>
+                </div>
+              )
+            })
+          )}
+        </div>
+      </div>
+    </>
   )
 }
 
---
+++ util/shared.js
@@ -0,0 +1,218 @@
+import SuccessImg from "data-base64:~assets/success.svg"
+import ErrorImg from "data-base64:~assets/error.svg"
+import WarnImg from "data-base64:~assets/warn.svg"
+
+const promisify = (method, ctx) => {
+    return function() {
+        var args = Array.prototype.slice.call(arguments, 0);
+        ctx = ctx || this;
+        return new Promise(function (resolve, reject) {
+            var callback = function () {
+                return function (result) {
+                    if (result) {
+                        return resolve(result);
+                    }
+                    return reject();
+                };
+            }
+            args.push(callback());
+            method.apply(ctx, args);
+        });
+    };
+};
+
+// copy text to clipboard
+export function copy(value) {
+    const copy = document.createElement('input');
+    document.body.appendChild(copy);
+    copy.value = value;
+    copy.select();
+    document.execCommand('copy');
+    document.body.removeChild(copy);
+}
+
+// cut String's first dot, e.g. .a.com => a.com
+export function cutFirstDot(str) {
+    return String(str).replace(/^\./, '');
+}
+
+// generate the given cookieObj's url
+export function generateURL(cookieObj) {
+    const { secure, domain, path } = cookieObj;
+    return  `http${secure ? 's' : ''}://${cutFirstDot(domain)}${path}`;
+}
+
+export const getCookiesAsync = promisify(chrome.cookies.getAll);
+
+export const getSingleCookieAsync = promisify(chrome.cookies.get);
+
+export const removeCookiesAsync = promisify(chrome.cookies.remove);
+
+export const setCookiesAsync = async function(cookie, isInCurrentDoamin) {
+    const adaptedCookie = { ...cookie };
+
+    debugger
+
+    // ignore hostOnly & session
+    delete adaptedCookie.hostOnly;
+    delete adaptedCookie.session;
+
+    // must set a correct url
+    adaptedCookie.url = generateURL(adaptedCookie);
+
+    // if path exist, it must equal to current tab's path, so force to set '/'
+    adaptedCookie.path = '/';
+
+    /**
+     * ensure the accuracy of domain attribute!
+     * see https://github.com/electron/electron/issues/17564#issuecomment-479032055
+     * If the domain-attribute is non-empty:
+     *   Set the cookie's host-only-flag to false
+     * Otherwise:
+     *   Set the cookie's host-only-flag to true
+     */
+    if (!cookie.domain.startsWith('.')) {
+        delete adaptedCookie.domain;
+    }
+
+    // set cookie in current active tab
+    if (isInCurrentDoamin) {
+        const tabs = await chrome.tabs.query({ currentWindow: true, active: true });
+        const host = new URL(tabs?.[0].url).host;
+        const hostWithoutPort = host.split(':')[0];
+        adaptedCookie.url = `http${adaptedCookie.secure ? 's' : ''}://${hostWithoutPort}/`;
+
+        if (/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(:\d+)?$/.test(hostWithoutPort) || hostWithoutPort.includes('localhost')) {
+            delete adaptedCookie.domain;
+        } else {
+            adaptedCookie.domain = `.${host}`;
+        }
+    }
+
+    return new Promise(function (res, rej) {
+        chrome.cookies.set(adaptedCookie).then(newCookie => {
+            if (!newCookie) {
+                console.log('error in chrome.cookies.set:', adaptedCookie);
+                return rej();
+            }
+            console.log('success cookie:', adaptedCookie);
+            return res(newCookie);
+        }).catch(() => {
+            return rej();
+        })
+    })
+}
+
+// get cookies of current tab's domain (and its father domain)
+export function getCookies() {
+    return chrome.tabs.query({ currentWindow: true, active: true }).then(tabs => {
+        if (!tabs?.[0]) {
+            return Promise.reject('can not get tabs');
+        }
+        return new URL(tabs[0].url).host;
+    }).then(async domain => {
+        // ip/localhost
+        if (/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(:\d+)?$/.test(domain) || domain.includes('localhost')) {
+            const domainWithoutPort = domain.split(':')[0];
+            const cookies = await getCookiesAsync({ domain: domainWithoutPort });
+            // return cookies || [];
+            return cookies.sort((a, b) => {
+                // domain lenght first, then name first
+                if (a.domain.length > b.domain.length) {
+                    return -1;
+                } else if (a.domain.length === b.domain.length) {
+                    if (a.name > b.name) {
+                        return 1;
+                    } else {
+                        return -1;
+                    }
+                } else {
+                    return 1;
+                }
+            });
+        }
+
+        // regular domain name
+        const rootDomain = domain.split('.').slice(-2).join('.');
+        const childDomain = domain.split('.').slice(0, -2);
+    
+        const targetDomainList = [rootDomain];
+
+        while(childDomain.length) {
+          const dm = childDomain.pop();
+          const child = [dm, targetDomainList[0]].join('.');
+            targetDomainList.unshift(child);
+        }
+
+        const cookies = await getCookiesAsync({ domain: rootDomain });
+
+        return cookies.filter(el => {
+            const elWithoutPot = cutFirstDot(el.domain);
+            return targetDomainList.includes(elWithoutPot);
+        }).sort((a, b) => {
+            // domain lenght first, then name first
+            if (a.domain.length > b.domain.length) {
+                return -1;
+            } else if (a.domain.length === b.domain.length) {
+                if (a.name > b.name) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            } else {
+                return 1;
+            }
+        });
+    });
+}
+
+export function Message(opt = {}) {
+    const { type, text } = opt;
+
+    const $box = document.createElement('div');
+    $box.classList = 'co-msg';
+
+    const $img = document.createElement('img');
+    const imgMap = {
+        success: SuccessImg,
+        warn: WarnImg,
+        error: ErrorImg,
+    }
+    $img.src = imgMap[type] || SuccessImg;
+
+    const $text = document.createElement('div');
+
+    $text.innerText = text || 'success~';
+
+    $box.appendChild($img);
+    $box.appendChild($text);
+
+    document.body.appendChild($box);
+
+    setTimeout(() => {
+        $box.classList = 'co-msg is-leaving';
+        // multi props trigger
+        $box.addEventListener('transitionend', () => {
+            document.body.contains($box) && document.body.removeChild($box);
+        });
+    }, 2000);
+}
+
+export function Loading() {
+    const $mask = document.createElement('div');
+    $mask.classList = 'co-loading';
+
+    const $text = document.createElement('div');
+
+    $mask.appendChild($text);
+
+    document.body.appendChild($mask);
+
+    $mask.close = () => {
+        setTimeout(() => {
+            document.body.contains($mask) && document.body.removeChild($mask);
+        }, 300);
+    }
+
+    return $mask;
+}
